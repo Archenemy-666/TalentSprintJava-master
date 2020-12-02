753bcc2a52fa05f9f123c84013128509ecf0074a
Arch-enemy <csiddu98@gmail.com>
2020-12-02 10:30:39 +0530

    knights path optimized
    shows no.of possible moves at every position on the board and the board is expandable too .

--- Workspace/TalentSprint/src/homework/src/chess/KnightPath.java
+++ Workspace/TalentSprint/src/homework/src/chess/KnightPath.java
@@ -8,20 +8,28 @@ public class KnightPath{
 
         int[][] board = new int[size][size] ;
 
-        System.out.println("enter x and y coordinates ranging from 0 to 7");
 
-        int x = scan.nextInt() ;
-        int y = scan.nextInt();
-        System.out.println(steps(board , x , y));
+        for(int x = 0 ; x< board.length ; x++){
+            for(int y = 0 ; y < board.length ; y++){
+                steps(board , x , y);
+                System.out.print(" " + board[x][y]);
+
+            }
+            System.out.println();
+        }
+        System.out.println("enter x and y coordinates ranging according to the size of board ");
+        int l = scan.nextInt() ;
+        int m  = scan.nextInt();
+        System.out.println(steps(board , l , m));
 
     }
 
-    public static int up(int[][] board , int x , int y) {
+    public static int up( int x , int y ,int size ) {
         int count = 0 ;
         int newUpX1 = x+1 ;
         int newUpY1 = y-2 ;
         int newUpX2 = x-1 ;
-        if(newUpX1 < 8 && newUpY1 >= 0){
+        if(newUpX1 < size && newUpY1 >= 0){
             count = count + 2 ;
         }
         if(newUpX2 >= 0 && newUpY1  >= 0){
@@ -29,39 +37,39 @@ public class KnightPath{
         }
         return count ;
     }
-    public static int down(int[][] board , int x , int y ){
+    public static int down( int x , int y ,int size){
         int count = 0 ;
         int newDownX1 = x+1 ;
         int newDownX2 = x-1 ;
         int newDownY1 = y+2 ;
 
-        if(newDownX1 < 8 && newDownY1<8){
+        if(newDownX1 < size && newDownY1<size){
             count = count + 2 ;
         }
-        if(newDownX2 >= 0 && newDownY1<8){
+        if(newDownX2 >= 0 && newDownY1<size){
             count = count + 2 ;
         }
         return count ;
     }
-    public static int right(int[][] board , int x , int y){
+    public static int right( int x , int y , int size){
         int count = 0 ;
         int newRX1 = x + 2;
         int newRY1 = y + 1;
         int newRY2 = y - 2;
-        if(newRX1 < 8 && newRX1 < 8){
+        if(newRX1 < size && newRX1 < size){
             count = count + 2 ;
         }
-        if(newRX1 < 8 && newRY2 >= 0 ){
+        if(newRX1 < size && newRY2 >= 0 ){
             count = count + 2 ;
         }
         return  count ;
     }
-    public static int left(int[][] board , int x , int y){
+    public static int left( int x , int y , int size){
         int count = 0 ;
         int newLX1 = x - 2 ;
         int newLY1 = y + 1 ;
         int newLY2 = y - 1 ;
-        if(newLX1 >= 0 && newLY1 < 8){
+        if(newLX1 >= 0 && newLY1 < size){
             count = count+2 ;
         }
         if(newLX1 >= 0 && newLY2 >= 0 ){
@@ -71,20 +79,21 @@ public class KnightPath{
     }
     public static int steps(int[][] board , int x , int y){
         int count = 0 ;
-        int countUp = up(board , x , y);
-        int countDown = down(board , x , y);
-        int countRight = right(board , x , y);
-        int countLeft = left(board , x , y);
+        int countUp = up( x , y , board.length);
+        int countDown = down( x , y, board.length);
+        int countRight = right( x , y , board.length);
+        int countLeft = left( x , y , board.length);
 
         count = countUp+countDown+countLeft+countRight ;
         board[x][y] = count ;
+        /*
         for(int i = 0 ; i < board.length ; i++){
             for(int j = 0 ; j< board.length ; j++){
-                System.out.print(board[i][j]);
+                System.out.print(" " + board[i][j]);
             }
             System.out.println();
         }
-
+        */
         return count;
     }
 
